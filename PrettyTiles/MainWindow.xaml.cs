af17d1589b1b3ff374544c39e6b50f68373285a3
Valters Tomsons <infinity1188@yahoo.com>
2017-04-22 03:32:56 +0300

    Basic file discovery and shell integration

--- PrettyTiles/MainWindow.xaml.cs
+++ PrettyTiles/MainWindow.xaml.cs
@@ -1,28 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using System.Diagnostics;
+using System.IO;
+using Shell32;
 
 namespace PrettyTiles
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
+        private static string _appdata = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+        private static List<string> ShortcutList = new List<string>();
+        private static string _programs = $"{_appdata}\\Microsoft\\Windows\\Start Menu\\Programs\\";
+
         public MainWindow()
         {
             InitializeComponent();
+            ExploreShortcuts();
+            LoadShortcuts();
+        }
+
+        //Discover the shortcuts in start menu
+        static void ExploreShortcuts()
+        {
+            foreach (string file in Directory.EnumerateFiles(_programs))
+            {
+                string lnk = file;
+                if(lnk.Contains(".lnk"))
+                {
+                    lnk = lnk.Replace(_programs, String.Empty);
+                    lnk = lnk.Replace(".lnk", String.Empty);
+                    Console.WriteLine($"{lnk} added to internal list");
+                    ShortcutList.Add(lnk);
+                }
+            }
+        }
+
+        //Load shortcut list into the ItemList control
+        void LoadShortcuts()
+        {
+            foreach(string foo in ShortcutList)
+            {
+                IconList.Items.Add(foo);
+            }
+        }
+
+        private void GithubIcon_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start("https://github.com/FaithLV");
+        }
+
+        //Get target path from shortcut (lnk)
+        //Should work for all windows versions, because 2006
+        //http://www.saunalahti.fi/janij/blog/2006-12.html#d6d9c7ee-82f9-4781-8594-152efecddae2
+        private string GetTargetPath(string _lnk)
+        {
+            Shell shell = new ShellClass();
+            Folder folder = shell.NameSpace(_programs);
+            FolderItem item = folder.ParseName($"{_lnk}.lnk");
+            ShellLinkObject link = (ShellLinkObject)item.GetLink;
+            return link.Path;
+        }
+
+        private void IconList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            //Full path to the shortcut
+            string _linkpath = $"{_programs}{IconList.SelectedItem.ToString()}.lnk";
+
+            //Full path to the target file
+            string _filepath = GetTargetPath(IconList.SelectedItem.ToString());
+
+            //Update the "Target Path" textbox
+            TargetTextBox.Text = _filepath;
         }
     }
 }
